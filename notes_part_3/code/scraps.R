2dab488fdeb121cd25327d5163b97d365998f4af
bob builder <ian@tagroup.co.nz>
2020-10-06 15:42:43 +1100

    update

---
+++ notes_part_3/code/scraps.R
@@ -0,0 +1,9 @@
+# # get any required packages
+# packs<-c("tidyr" ,"dplyr","RCurl") #
+# remove.packages(packs)
+# packs_not_installed <- packs[!(packs %in% installed.packages()[,"Package"])]
+# if(length(packs_not_installed)>0) {install.packages(packs_not_installed)}
+# eval(parse(text=paste0("library(",packs,")")))
+# eval(parse(text=paste0("detach('package:",packs,"',unload=TRUE)")))
+#install.packages("RCurl")
+#library(RCurl)
